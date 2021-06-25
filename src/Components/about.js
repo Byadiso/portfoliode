98f1af3f1130cd259696b84614e19bf58b3efbaf
Byadiso <byadiso@gmail.com>
2021-06-26 00:45:15 +0200

    fixing-bug-style-again

--- src/Components/about.js
+++ src/Components/about.js
@@ -6,29 +6,29 @@ class Aboutme extends Component{
         return(
         <div className='aboutme-body'>
              <Grid className='aboutme-grid'>
-                <Cell col={6}> 
+                <Cell col={3}> 
                      {/* <h2>  Meet Him</h2> */}
-                    <img src="./image/desire.jpg " alt= "deesire" style={ {height: '200px', borderRadius: '50%', paddingTop:'3.0em'}}/>                   
-                    <h6>   BYAMUNGU Desire</h6>
+                    <img src="./image/desire.jpg " alt= "deesire" style={ {height: '140px',width: '140px',borderRadius: '50%', paddingTop:'2.0em'}}/>                   
+                    <h6> BYAMUNGU Desire</h6>
                    
-                    </Cell>
+                </Cell>
 
-                <Cell className="aboutme-right-col" col={6}>
+                <Cell className="aboutme-right-col" col={9}>
                     
                                     
                 <div  className="about-list" >
                     <List>
                             <ListItem>
-                                <ListItemContent style={{fontSize: '20px', color: 'grey'}} >                                
-                              "He is spending his time creating solutions using javascript and recent technologies! 
-                               when not coding he likes playing his guitar,and sometime modelling 3D character for games with Blender. 
+                                <ListItemContent style={{fontSize: '17px', color: 'grey'}} >                                
+                              "He is spending his time creating things using javascript!When not coding he likes playing his guitar,and sometime modelling 3D characters for games with Blender. 
                                "
                                </ListItemContent>
                             </ListItem>
+                            <p style= {{ width: '100%', margin:'0px' ,fontStyle:'italic'}}>A young creator, team player, optimist and a CEO at Ngana Tech .</p>
                            
                     </List>
-                    <p style= {{ width: '103%', margin:'auto' ,fontStyle:'italic'}}>A young creator, team player, optimist and a CEO at Ngana Tech .</p>
-                        </div>
+                    
+                </div>
                     
                 </Cell>
               
